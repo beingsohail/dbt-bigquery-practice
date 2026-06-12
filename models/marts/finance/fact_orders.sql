8ca49cd9399b836f9b9e8e8e6937c71ea2f14342
Mohammad Sohail Shekh <edu.sohail18@gmail.com>
2026-06-12 21:11:12 +0000

    Fixed structural issues.

--- models/marts/finance/fact_orders.sql
+++ models/marts/finance/fact_orders.sql
@@ -1,6 +1,6 @@
 select 
-    id as order_id,
+    o.id as order_id,
     user_id as customer_id,
     amount
 from `dbt-tutorial.jaffle_shop.orders` o
-left join {{ ref('stg_stripe_payments') }} on o.orders_id = p.orderid
+left join {{ ref('stg_stripe_payments') }} p on o.id = p.orderid
