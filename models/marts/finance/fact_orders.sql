9d22f1c7279012eabc8add906092b9e2160ac480
Mohammad Sohail Shekh <edu.sohail18@gmail.com>
2026-06-12 21:29:08 +0000

    fixed issues in fact_orders

--- models/marts/finance/fact_orders.sql
+++ models/marts/finance/fact_orders.sql
@@ -1,6 +1,30 @@
-select 
-    o.id as order_id,
-    user_id as customer_id,
-    amount
-from `dbt-tutorial.jaffle_shop.orders` o
-left join {{ ref('stg_stripe_payments') }} p on o.id = p.orderid
+with orders as (
+    select 
+        *
+    from {{ ref('stg_jaffle_shop_orders') }}
+),
+
+payment as (
+    select *
+    from {{ ref('stg_stripe_payments') }}
+),
+
+orders_payment as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount else 0 end) as total_amount
+    from payment
+    group by 1
+),
+
+final as (
+    select 
+        o.order_id,
+        o.customer_id,
+        o.order_date,
+        coalesce(op.total_amount, 0) as amount
+    from orders o
+    left join orders_payment op using(order_id)
+)
+
+select * from final
--- models/staging/stripe/stg_stripe_payments.sql
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -1,2 +1,18 @@
-select *
-from `dbt-tutorial.stripe.payment`
+with source as (
+    select *
+    from `dbt-tutorial.stripe.payment`
+),
+
+renamed as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount,
+        created as payment_date,
+        _batched_at
+    from source 
+)
+
+select * from renamed
